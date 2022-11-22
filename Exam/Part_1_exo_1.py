e87b8203ab9f2c2f6e56297d141d9c8616c6980c
N3659 <76428379+N3659@users.noreply.github.com>
2022-11-22 14:34:26 +0000

    Add files via upload

--- Exam/Part_1_exo_1.py
+++ Exam/Part_1_exo_1.py
@@ -1,10 +1,8 @@
 myTable = [15, 95, 17, 34]
 save = 0
-plusGrand = 0
 
-for i in range (len(myTable)):
-    if myTable[i]<myTable[i-1]:
-            save = myTable[i]
-            myTable[i]=myTable[i-1]
-            myTable[i-1]=save
+save = myTable[0]
+myTable[0] = myTable[2]
+myTable[2]= save
+
 print(myTable)
